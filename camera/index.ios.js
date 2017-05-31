a479e0c3d60f6eba60a6bd0ff8c9e78c3c309571
Zak Campuzano <owc@lawn-143-215-113-213.lawn.gatech.edu>
2017-05-31 19:11:39 -0400

    initial commit

---
+++ camera/index.ios.js
@@ -0,0 +1,79 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  Image,
+  TouchableHighlight,
+  TouchableOpacity,
+  View
+} from 'react-native';
+
+import Camera from 'react-native-camera';
+
+class BadInstagramCloneApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+          <Text style={styles.metric} >Select metric...</ Text>
+          <TouchableOpacity onPress={this.takePicture.bind(this)}>
+            <Image
+              style={styles.capture}
+              source={require('./img/play.png')}
+            />
+          </TouchableOpacity>
+
+        </Camera>
+      </View>
+    );
+  }
+
+  takePicture() {
+    const options = {};
+    //options.location = ...
+    this.camera.capture({metadata: options})
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+  }
+
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'flex-start'
+  },
+  capture: {
+    flex: 0,
+    width: 75,
+    height: 75,
+    padding: 0,
+    margin: 0
+
+  },
+  metric: {
+    flex: 0,
+    backgroundColor: '#000',
+    color: '#fff',
+    margin: 10
+
+  }
+});
+
+AppRegistry.registerComponent('camera', () => BadInstagramCloneApp);
